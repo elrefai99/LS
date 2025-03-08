7ba5a324a14158cb6254f612611e8bd0f37bf326
elrefai99 <mohamed.mostafa0699@gmail.com>
2025-03-08 19:27:28 +0200

    chore: update deps

---
+++ backend/src/app.ts
@@ -0,0 +1,18 @@
+import "dotenv/config"
+import express, { Application, Request, Response } from 'express'
+import { connectToDatabase } from './Database/PostgreSQL'
+import setupUtils from './utils/setup.utils'
+
+const app: Application = express()
+
+setupUtils(app)
+
+app.use('*', async (_req: Request, res: Response) => {
+  res.status(404).send('This is not the API route you are looking for')
+  return
+})
+
+app.listen(process.env.PORT as string, () => {
+  connectToDatabase()
+  console.log("Server is running on:", `http://localhost:${process.env.PORT as string || 8000}`)
+})
