a6a10088524b54adeb72b0e326e5b1f25b20a671
elrefai99 <mohamed.mostafa0699@gmail.com>
2025-04-21 17:20:07 +0200

    chore: set new setup of shor

--- src/app.ts
+++ src/app.ts
@@ -3,4 +3,6 @@ import express from 'express';
 
 const app = express();
 
-app.listen()
+app.listen(process.env.PORT, () => {
+  console.log(`🖥️  Server is running on port ${process.env.PORT}`);
+});
